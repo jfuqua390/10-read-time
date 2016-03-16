6e657bdfb555df4f7d51297cbd56aefea9f4fb17
Jonathan Fuqua <jfuqua390@gmail.com>
2016-03-16 17:55:39 -0500

    Goal complete hard mode

--- src/index.js
+++ src/index.js
@@ -3,12 +3,21 @@
 
 const entryinfos = document.querySelectorAll(`.entry__info`);
 const entrys = document.querySelectorAll(`.entry`);
+const title = document.querySelector(`.title`);
+
+// const to use in insertBefore, uncomment to move nav top
+// const body = document.querySelector(`body`);
+// const tes = document.getElementByID(`lua-cooper`);
 
 // Create nav
 const blog = document.querySelector(`.blog`);
 const nav = document.createElement(`nav`);
 nav.classList.add(`navbar`);
 blog.appendChild(nav);
+
+// to move nav to top, uncomment next line
+// body.insertBefore(nav, title);
+
 
 for (let i = 0; i < entrys.length; i++) {
   // Making each element
@@ -22,7 +31,13 @@ for (let i = 0; i < entrys.length; i++) {
   const article = entry.querySelector(`.entry__content`).textContent;
   readtime.innerText = `${readTime(article, 200)} mins`;
 
-  //create nav links
+  if (readTime(article, 200) < 6) {
+    entry.querySelector(`.read-time`).style.color = `green`;
+  } else {
+    entry.querySelector(`.read-time`).style.color = `red`;
+  }
+
+  // create nav links
   const navlink = document.createElement(`a`);
   navlink.setAttribute(`href`, `#${entry.id}`);
   nav.appendChild(navlink);
