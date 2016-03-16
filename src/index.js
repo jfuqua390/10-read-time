b7acaa889fa14dca3f5f888b6f47d84d84a796e9
Jonathan Fuqua <jfuqua390@gmail.com>
2016-03-16 16:31:26 -0500

    Goal Show readtimes to user

--- src/index.js
+++ src/index.js
@@ -1,2 +1,21 @@
 'use strict';
 /* globals numWords, readTime */
+
+const entryinfos = document.querySelectorAll(`.entry__info`);
+const entrys = document.querySelectorAll(`.entry`);
+
+for (let i = 0; i < entrys.length; i++) {
+  // Making each element
+  const entryinfo = entryinfos[i];
+  const readtime = document.createElement(`li`);
+  readtime.classList.add(`read-time`);
+  entryinfo.appendChild(readtime);
+
+  // Calculating readTime
+  const entry = entrys[i];
+  const article = entry.querySelector(`.entry__content`).innerText;
+  readtime.innerText = `${readTime(article, 200)} mins`;
+
+  // Creating nav
+
+}
