0899fc2db61aa937a738a5610acb5f76064a7828
Jonathan Fuqua <jfuqua390@gmail.com>
2016-03-17 09:05:29 -0500

    Final markup and styles

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ const entrys = document.querySelectorAll(`.entry`);
 const title = document.querySelector(`.title`);
 
 // const to use in insertBefore, uncomment to move nav top
-// const body = document.querySelector(`body`);
+const bodyEl = document.querySelector(`body`);
 // const tes = document.getElementByID(`lua-cooper`);
 
 // Create nav
@@ -16,7 +16,7 @@ nav.classList.add(`navbar`);
 blog.appendChild(nav);
 
 // to move nav to top, uncomment next line
-// body.insertBefore(nav, title);
+bodyEl.insertBefore(nav, title);
 
 
 for (let i = 0; i < entrys.length; i++) {
@@ -31,6 +31,7 @@ for (let i = 0; i < entrys.length; i++) {
   const article = entry.querySelector(`.entry__content`).textContent;
   readtime.innerText = `${readTime(article, 200)} mins`;
 
+  // hard mode if statement to change color based on read-time
   if (readTime(article, 200) < 6) {
     entry.querySelector(`.read-time`).style.color = `green`;
   } else {
